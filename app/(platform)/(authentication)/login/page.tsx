db477253e60d157ed89569d7d71de0434f05defe
Carlos Araiza <eliptikman1234@gmail.com>
2023-12-15 23:38:38 -0600

    Update login page with Google sign-in option

--- app/(platform)/(authentication)/login/page.tsx
+++ app/(platform)/(authentication)/login/page.tsx
@@ -1,14 +1,21 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+
 export default function HomePage() {
   return (
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-          <h2>This is it</h2>
-          <form className="flex flex-col">
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" />
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" />
-            <button type="submit">Sign in</button>
-          </form>
-        </div>
-    )
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+      <h2>This is it</h2>
+      <button onClick={() => {
+        signIn("google");
+      }}>Loggin with google</button>
+      <form className="flex flex-col">
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+        <button type="submit">Sign in</button>
+      </form>
+    </div>
+  )
 }
