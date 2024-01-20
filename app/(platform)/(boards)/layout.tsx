27f0c9d0485f1a827393a484de040d297a1dab14
Carlos Araiza <eliptikman1234@gmail.com>
2024-01-19 20:10:59 -0600

    Add useIsAuth hook to layout component

--- app/(platform)/(boards)/layout.tsx
+++ app/(platform)/(boards)/layout.tsx
@@ -5,6 +5,7 @@ import { getCookie, setCookie } from "cookies-next";
 import { SessionProvider, useSession } from 'next-auth/react'
 import { redirect } from 'next/navigation'
 import { Navbar } from '@/components/Navbar'
+import { useIsAuth } from '@/hooks/useIsAuth';
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
     const [loading, setLoading] = React.useState(true)
@@ -94,41 +95,6 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
         return (
             <h1>Loading</h1>
         )
-    }
-}
-
-function useIsAuth($token: any): any {
-    const [isLoadded, setIsLoadded] = React.useState(true)
-    const [isAuthenticated, setIsAuthenticated] = React.useState(false)
-    const [error, setError] = React.useState(false)
-
-    var myHeaders = new Headers();
-    myHeaders.append("Accept", "application/json");
-    myHeaders.append("Authorization", "Bearer " + $token);
-
-    fetch("http://api_taski.test/api/user", {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-    })
-        .then(response => response.json())
-        .then(result => {
-            console.log('result', result.message)
-            if (result.data) {
-                setIsAuthenticated(true)
-            } else {
-                setIsAuthenticated(false)
-            }
-        })
-        .catch(error => error)
-        .finally(() => {
-            setIsLoadded(false)
-        });
-
-    return {
-        isLoadded,
-        isAuthenticated,
-        error
     }
 }
 
