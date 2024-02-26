37f8cc140a13d2fb3917a3b4e845d389261b496c
Carlos Araiza <eliptikman1234@gmail.com>
2024-02-25 23:26:49 -0600

    Refactor Hero component and update homepage content

--- app/(Homepage)/component/Hero.tsx
+++ app/(Homepage)/component/Hero.tsx
@@ -82,8 +82,7 @@ export const Hero = () => {
                     <div className="max-w-2xl mx-auto text-center">
                         <h2 className="text-2xl font-bold text-gray-800 sm:text-4xl sm:leading-tight">Trusted by world class companies, design teams & popular designers</h2>
                     </div>
-                    {
-                        /*
+
                     <div className="grid items-center max-w-4xl grid-cols-2 mx-auto mt-12 md:mt-20 md:grid-cols-4 gap-x-10 gap-y-16">
                         <div>
                             <img className="object-contain w-full h-6 mx-auto" src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-1.png" alt="" />
@@ -133,8 +132,6 @@ export const Hero = () => {
                             <img className="object-contain w-full h-8 mx-auto" src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-12.png" alt="" />
                         </div>
                     </div>
-                            */
-                    }
 
                     <div className="flex items-center justify-center mt-10 space-x-3 md:hidden">
                         <div className="w-2.5 h-2.5 rounded-full bg-blue-600 block"></div>
